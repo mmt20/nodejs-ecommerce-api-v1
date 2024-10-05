6c6574b85a1d85bd6c97cd7f9eda6a25b27b9fc0
mmt20 <mmt202002@gmail.com>
2024-10-05 18:27:07 +0300

    Finish User Addresses

--- server.js
+++ server.js
@@ -17,6 +17,7 @@ const userRoute = require('./routes/userRoute');
 const authRoute = require('./routes/authRoute');
 const reviewRoute = require('./routes/reviewRoute');
 const wishlistRoute = require('./routes/wishlistRoute');
+const addressRoute = require('./routes/addressRoute');
 
 // connect with db
 dbConnection();
@@ -41,6 +42,7 @@ app.use('/api/v1/users', userRoute);
 app.use('/api/v1/auth', authRoute);
 app.use('/api/v1/reviews', reviewRoute);
 app.use('/api/v1/wishlist', wishlistRoute);
+app.use('/api/v1/addresses', addressRoute);
 
 app.all('*', (req, res, next) => {
   // Create a 404 error and pass it to the global error handler
